022fa889a694d83829d73e423cf6c7ddb944c460
Saad-SYEDK <saadsyedmd@gmail.comgit config --listgit config --global user.email saadsyedmd@gmail.com>
2024-09-29 17:52:25 +0530

    Solved 2 Problems(reverse_string, parenthesis-matching)

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -23,6 +23,60 @@ class Stack:
         return len(self.container) #Return the length of the stack
     
     def is_empty(self):
-        return len(self.container) == 0 #if length is 0 then stack is empty
+        return len(self.container) == 0 #if length is 0 then stack is empty.
+    
+#Questions:
+    
+#Q1. Write a function that can reverse a string usign the stack class.
+def reverse_string(str):
+    # Intuition: we can push each char of the string into stack.
+    # Once the string is over, we pop each char and append into variable
+    
+    stack = Stack() # initialize stack
+    for char in str:
+            stack.push(char)
+    
+    #initialize a variable to store our answer
+    ans = ""
+    
+    #pop each element in the stack and append into our answer
+    while not stack.is_empty():
+        ans += stack.pop()
+    
+    return ans
+
+#Q2. Write a fuction that  checks if parenthesis in the string are balanced or not.
+# Possible parenthesis are "{}',"()" or "[]".
+def is_balanced(str):
+    #Intuition : Whenever we find an opening parenthesis we will push it onto stack
+    # when we find a closing parenthesis we pop from our stack and compare, if they do not match
+    # it means the string is not balanced, after itrating the strign if the stack is not 
+    # not empty then also the string is not balanced
+    
+    stack = Stack()
+    
+    for char in str:
+        
+        # if opening bracket - push into stack
+        if char == '{' or char == '(' or char == '[':
+            stack.push(char)
+        
+        # if closing bracket - pop and compare
+        if char == '}':
+            if stack.pop() != '{':
+                return False
+        elif char == ')':
+            if stack.pop() != '(':
+                return False
+        elif char ==']':
+            if stack.pop() != '[':
+                return False
+    if stack.is_empty():
+        return True
+    else:
+        return False
+
+# Checking
+print(is_balanced("[a+b]*(x+2y)@@@*{gg+kk}"))
     
     
